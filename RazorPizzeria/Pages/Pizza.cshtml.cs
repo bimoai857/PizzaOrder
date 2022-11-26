37f849a2bd9ed103e3c688a1a064dd14fd89180d
Bimochan <bimoai857@gmail.com>
2022-11-26 18:51:21 +0545

    first commit

---
+++ RazorPizzeria/Pages/Pizza.cshtml.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Models;
+
+namespace RazorPizzeria.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public List<PizzasModel> fakePizzaDB = new List<PizzasModel>()
+        {
+            new PizzasModel()
+            {
+                ImageTitle="Margerita",
+                PizzaName="Margerita",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new PizzasModel()
+            {
+                ImageTitle="Bolognese",
+                PizzaName="Bolognese",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new PizzasModel()
+            {
+                ImageTitle="Hawaiian",
+                PizzaName="Hawaiian",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new PizzasModel()
+            {
+                ImageTitle="Carbonara",
+                PizzaName="Carbonara",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            },
+            new PizzasModel()
+            {
+                ImageTitle="Mushroom",
+                PizzaName="Mushroom",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4
+            }
+            };
+
+        public void OnGet()
+        {
+        }
+    }
+}
